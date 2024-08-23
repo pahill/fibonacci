d6212ad54ccf6165ef481d7298fc7dabd3372aac
pahill <pamela.a.hill@gmail.com>
2024-08-23 15:21:46 +0200

    Fibonacci all the things

--- convention-plugins/src/main/kotlin/root.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/root.publication.gradle.kts
@@ -3,8 +3,8 @@ plugins {
 }
 
 allprojects {
-    group = "org.jetbrains.kotlinx.multiplatform-library-template"
-    version = "0.0.1"
+    group = "io.github.pahill"
+    version = "1.0.3"
 }
 
 nexusPublishing {
