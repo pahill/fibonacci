2a1c975dc1b26be5ece4fd854310ac7d2089b34e
pahill <pamela.a.hill@gmail.com>
2024-08-26 09:25:50 +0200

    Fibonacci deploy 3

--- fibonacci/build.gradle.kts
+++ fibonacci/build.gradle.kts
@@ -2,6 +2,7 @@ import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
+
 group = "io.github.pahill"
 version = "1.0.3"
 
@@ -44,5 +45,43 @@ android {
     compileSdk = libs.versions.android.compileSdk.get().toInt()
     defaultConfig {
         minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.DEFAULT)
+    // or when publishing to https://s01.oss.sonatype.org
+    publishToMavenCentral(SonatypeHost.S01)
+    // or when publishing to https://central.sonatype.com/
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
+
+    coordinates("io.github.pahill", "mylibrary-fibonacci", "1.0.3")
+
+    pom {
+        name.set("Fibonacci library")
+        description.set("A description of what my library does.")
+        inceptionYear.set("2024")
+        url.set("https://github.com/pahill/fibonacci/")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("pahill")
+                name.set("Pamela Hill")
+                url.set("https://github.com/pahill/")
+            }
+        }
+        scm {
+            url.set("https://github.com/pahill/fibonacci/")
+            connection.set("scm:git:git://github.com/pahill/fibonacci.git")
+            developerConnection.set("scm:git:ssh://git@github.com/pahill/fibonacci.git")
+        }
     }
 }
