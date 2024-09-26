7aff8f09f6ab0f5f438ba4909eda30025c33c1b4
pahill <pamela.a.hill@gmail.com>
2024-09-26 14:10:14 +0200

    Fibonacci - update versions

--- fibonacci/build.gradle.kts
+++ fibonacci/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 group = "io.github.pahill"
-version = "1.0.6"
+version = "1.0.7"
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
@@ -52,7 +52,7 @@ mavenPublishing {
 
     signAllPublications()
 
-    coordinates("io.github.pahill", "fibonacci", "1.0.6")
+    coordinates("io.github.pahill", "fibonacci", "1.0.7")
 
     pom {
         name.set("Fibonacci library")
